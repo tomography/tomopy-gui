194b82bbdd793ed0272313ee0928b45578641413
Francesco De Carlo <decarlof@gmail.com>
2017-05-13 23:23:14 -0500

    added DX loader and calibration

---
+++ tomopyui/util.py
@@ -0,0 +1,29 @@
+import h5py
+import dxchange
+
+def read_dx_dims(fname, dataset):
+    """
+    Read array size of a specific group of Data Exchange file.
+    Parameters
+    ----------
+    fname : str
+        String defining the path of file or file name.
+    dataset : str
+        Path to the dataset inside hdf5 file where data is located.
+    Returns
+    -------
+    ndarray
+        Data set size.
+    """
+
+    grp = '/'.join(['exchange', dataset])
+
+    with h5py.File(fname, "r") as f:
+        try:
+            data = f[grp]
+        except KeyError:
+            return None
+
+        shape = data.shape
+
+    return shape
